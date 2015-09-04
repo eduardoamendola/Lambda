a6ba468500e70189f34038a555781e9e079bd15b
Eduardo Amendola <eduardoamendola@gmailcom>
2015-09-04 15:38:59 +0100

    Adding lambda functions to set permissions for sns topic, since there's no way to do it with CFN

---
+++ Functions/TestFunction/index.js
@@ -0,0 +1,9 @@
+
+console.log('Loading function');
+
+exports.handler = function(event, context) {
+	console.log('Received event:', JSON.stringify(event, null, 2));
+	var message = event.Records[0].Sns.Message;
+	console.log('From SNS:', message);
+	context.succeed(message);
+};
